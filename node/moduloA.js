ac1e5dd89dac3b0d060a306f0475e7be0ad482d9
unknown <patosanta1000@gmail.com>
2023-02-22 14:57:04 -0300

    Conceitos node aplicados

---
+++ node/moduloA.js
@@ -0,0 +1,5 @@
+console.log(this)
+
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o proximo exemplo' 
---
+++ node/pastaB/acessarModulos.js
@@ -0,0 +1,17 @@
+const moduloA = require('../moduloA')
+console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+
+const c = require('./pastaC/index')
+console.log(c.ola2)
+
+// modulos core
+
+const http = require('http') 
+http.createServer((req, res) => {
+    res.write('Bom dia')
+    res.end()
+}).listen(8080)
