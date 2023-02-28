5ae1bc15b47b307a87c4bc882584ef812bdf31a4
unknown <patosanta1000@gmail.com>
2023-02-28 14:26:29 -0300

    Prática De conceitos node

--- node1/thisArrowFunction.js
+++ node1/thisArrowFunction.js
@@ -1,7 +1,7 @@
 function functionExpression() {
     console.log("Aponta para o global ->", this == global)
     console.log("Não aponta para o exports ->", this == exports)
-    console.log("Não aponta para o module.exports", this == module.exports)
+    console.log("Não aponta para o module.exports ->", this == module.exports)
 
     console.log("O this nessa função é um objeto Global ->", this)
 }
